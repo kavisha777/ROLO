27b8a2534d97da14c782171725fa912b37779296
kavisha777 <kaviyohen@gmail.com>
2025-06-07 13:32:07 +0530

    doneloginsignup

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  contact: { type: String,},
+  address: { type: String },
+  idProof: { type: String }, // file path or URL
+  role: { type: String, enum: ['user', 'seller', 'admin'], default: 'user' }
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
